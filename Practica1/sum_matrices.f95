51a9979915078211788489539c8b9d7dd21c2ceb
Pablo Monteagudo Lago <pmonteagudolago@gmail.com>
2019-02-05 16:01:36 +0100

    start diag

--- Practica1/sum_matrices.f95
+++ Practica1/sum_matrices.f95
@@ -3,6 +3,35 @@ program sum_matrices
 implicit none
 
 integer:: m, n, i, j
-real, allocatable:: a, b, s
+real, allocatable, dimension(:,:):: a, b, s
+
+print*, 'Numero de filas da matriz'
+read*, m
+print*, 'Numero de columnas da matriz'
+read*, n
+
+allocate(a(m,n), b(m,n), s(m,n))
+
+do i=1,m
+  do j=1,n
+    print*, 'A[ ', i, ' , ', j, ' ]: '
+    read*, a(i,j)
+  end do
+end do
+
+do i=1,m
+  do j=1,n
+    print*, 'B[ ', i, ' , ', j, ' ]: '
+    read*, b(i,j)
+  end do
+end do
+
+s = a + b
+
+do i=1,m
+  do j=1,n
+    print*, 'S[ ', i, ' , ', j, ' ]: ', s(i,j)
+  end do
+end do
 
 end program
---
+++ Practica1/tridiagxvector.f95
@@ -0,0 +1,32 @@
+program tridiagxvector
+
+implicit none
+
+integer:: n,i
+real, allocatable, dimension(:):: v, w, ad, al, au
+
+print*, 'Dame a dimension da matriz'
+read*, n
+
+allocate(ad(n), al(2:n). au(1:n-1), v(n), w(n))
+
+print*, 'Dame a diagonal principal'
+read*, ad(1:n)
+
+print*, 'Dame a diagonal superior'
+print*, au(1:n-1)
+
+print*, 'Dame a diagonal inferior'
+print*, al(2:n)
+
+print*, 'Dame o vector'
+print*, v(1:n)
+
+w = 0
+w(1:n) = ad*v(1:n)
+w(1:n-1)=w(1:n-1) + au*v(1: n-1)
+w(2:n) = w(2:n) + al*v(2:n)
+
+print*, 'Vector w: ', w
+
+end program
